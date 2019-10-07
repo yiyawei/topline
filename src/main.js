82751dbd265148600e4c194e2ac271ba15532b55
yiyawei <18519139511@163.com>
2019-10-07 20:13:22 +0800

    项目结构整理

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(elementUI)
 
 Vue.config.productionTip = false
 
